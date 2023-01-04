b4b6b0ca24f5e8b85d6417fd3413d1dd73aaf31b
fabio-codes-win <fabiohc1@hotmail.com>
2023-01-03 21:17:57 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import Button from "./Button"
+
+const Header = () => {
+  const onClick=()=>{
+    console.log()
+  }
+  return (
+    <header className="header">
+      <h1>Task Tracker</h1>
+      <Button color="green" text="Add" onClick={onClick}/>
+    </header>
+  )
+}
+
+export default Header
